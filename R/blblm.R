f1fbe851df4e420c9ab5bb5499ef39206a438399
slconnolly <slconnolly@ucdavis.edu>
2020-06-14 18:56:50 -0700

    here goes

--- R/blblm.R
+++ R/blblm.R
@@ -1,6 +1,10 @@
 #' @import purrr
 #' @import stats
+#' @import parallel
 #' @importFrom magrittr %>%
+#' @importFrom utils count.fields
+#' @importFrom utils capture.output
+#' @importFrom utils read.csv
 #' @details
 #' Linear Regression with Little Bag of Bootstraps
 "_PACKAGE"
@@ -11,12 +15,50 @@
 utils::globalVariables(c("."))
 
 
+#' @param formula the user specifies the independent and dependent variables in their analysis
+#' @param data the data frame or series of csv files with data
+#' @param m an integer indicating how many pieces to split the data into before bootstrap analysis
+#' @param B an integer indicating the number of bootstrapping procedures to perform
+#' @param parallelization a logical value indicating whether or not to use parallelization
+#' @param spec an integer to specify the number of cores to use while parallelizing
+#' @param model_choice the model to be used, only lm1 and glm1 are supported
+#'
+#' @return an object of class blblm, a bootstrapped linear model fitted to the provided data
+#' @example
+#' blblm(Sepal.Length~Sepal.Width, iris, m=20, B = 10000, parallelization = TRUE, spec = 3, model_choice = "lm1")
 #' @export
-blblm <- function(formula, data, m = 10, B = 5000) {
-  data_list <- split_data(data, m)
-  estimates <- map(
-    data_list,
-    ~ lm_each_subsample(formula = formula, data = ., n = nrow(data), B = B))
+blblm <- function(formula,
+                  data,
+                  m = 10, B = 5000,
+                  parallelization = FALSE,
+                  spec = 1,
+                  model_choice = "lm1") {
+  if (is.data.frame(data)) {
+    data_list <- split_data(data, m)
+    estimates <- map(data_list,
+                    ~ lm_each_subsample(
+                      formula = formula,
+                      data = .,
+                      n = nrow(data),
+                      B = B,
+                      model_choice = model_choice
+                    ))
+  } else {
+    # total number of observations over multiple files
+    n = sum(sapply(data, function(x) length(count.fields(x))))
+    estimates <- map(
+      data,
+      ~ lm_each_subsample(
+        formula = formula,
+        data = .,
+        n = n,
+        B = B,
+        parallelization = parallelization,
+        spec = spec,
+        model_choice = model_choice
+      )
+    )
+  }
   res <- list(estimates = estimates, formula = formula)
   class(res) <- "blblm"
   invisible(res)
@@ -29,37 +71,63 @@ split_data <- function(data, m) {
   data %>% split(idx)
 }
 
-
 #' compute the estimates
-lm_each_subsample <- function(formula, data, n, B) {
-  replicate(B, lm_each_boot(formula, data, n), simplify = FALSE)
-}
-
+lm_each_subsample <-
+  function(formula, data, n, B, parallelization = FALSE, spec = 1, model_choice = "lm1") {
+    if (parallelization) {
+      cl <- makeCluster(spec)
+      clusterExport(cl, c("lm_each_boot", "lm1", "blbcoef", "blbsigma"))
+      out <- parSapply(cl, seq_len(B), function(i) {
+        lm_each_boot(formula, data, n, model_choice)
+      })
+      stopCluster(cl)
+    } else {
+      out = replicate(B, lm_each_boot(formula, data, n, model_choice), simplify = FALSE)
+    }
+    return(out)
+  }
 
 #' compute the regression estimates for a blb dataset
-lm_each_boot <- function(formula, data, n) {
+lm_each_boot <- function(formula, data, n, model_choice) {
+  if (!is.data.frame(data)){
+    data = read.csv(data)
+  }
   freqs <- rmultinom(1, n, rep(1, nrow(data)))
-  lm1(formula, data, freqs)
+  if (model_choice == "lm1"){
+    lm1(formula, data, freqs)
+  } else {
+    glm1(formula, data, freqs)
+  }
 }
 
-
-#' estimate the regression estimates based on given the number of repetitions
+#' estimate the regression estimates based on the given number of repetitions
 lm1 <- function(formula, data, freqs) {
   # drop the original closure of formula,
-  # otherwise the formula will pick a wront variable from the global scope.
+  # otherwise the formula will pick a wrong variable from the global scope.
   environment(formula) <- environment()
   fit <- lm(formula, data, weights = freqs)
+  #fit <- lm_cpp(formula, data, weights = freqs)
+  list(coef = blbcoef(fit), sigma = blbsigma(fit))
+}
+
+#' estimate the LOGISTIC regression estimates based on the given number of repetitions
+glm1 <- function(formula, data, freqs) {
+  environment(formula) <- environment()
+  fit <- glm(formula, data, weights = freqs, family = "binomial")
+  #fit <- lm_cpp(formula, data, weights = freqs)
   list(coef = blbcoef(fit), sigma = blbsigma(fit))
 }
 
 
 #' compute the coefficients from fit
+
 blbcoef <- function(fit) {
   coef(fit)
 }
 
 
 #' compute sigma from fit
+
 blbsigma <- function(fit) {
   p <- fit$rank
   y <- model.extract(fit$model, "response")
@@ -71,6 +139,8 @@ blbsigma <- function(fit) {
 
 #' @export
 #' @method print blblm
+#' @param x an object of class blblm
+#' @return prints the blblm object's formula to the console
 print.blblm <- function(x, ...) {
   cat("blblm model:", capture.output(x$formula))
   cat("\n")
@@ -79,6 +149,10 @@ print.blblm <- function(x, ...) {
 
 #' @export
 #' @method sigma blblm
+#' @param object an object of class blblm
+#' @param confidence logical value (default false) wherein the user chooses whether to return the confidence interval or just the sigma
+#' @param level a double value between 0 and 1 signifying the alpha level desired
+#' @return the sigma of a blblm object and, if confidence = TRUE, its confidence interval
 sigma.blblm <- function(object, confidence = FALSE, level = 0.95, ...) {
   est <- object$estimates
   sigma <- mean(map_dbl(est, ~ mean(map_dbl(., "sigma"))))
@@ -95,6 +169,8 @@ sigma.blblm <- function(object, confidence = FALSE, level = 0.95, ...) {
 
 #' @export
 #' @method coef blblm
+#' @param object an object of class blblm
+#' @return the (Intercept) and coefficient of the independent variables inside the blblm model created previously
 coef.blblm <- function(object, ...) {
   est <- object$estimates
   map_mean(est, ~ map_cbind(., "coef") %>% rowMeans())
@@ -103,9 +179,11 @@ coef.blblm <- function(object, ...) {
 
 #' @export
 #' @method confint blblm
+#' @param object an object of class blblm
+#' @param parm
 confint.blblm <- function(object, parm = NULL, level = 0.95, ...) {
   if (is.null(parm)) {
-    parm <- attr(terms(fit$formula), "term.labels")
+    parm <- attr(terms(object$formula), "term.labels")
   }
   alpha <- 1 - level
   est <- object$estimates
@@ -121,6 +199,11 @@ confint.blblm <- function(object, parm = NULL, level = 0.95, ...) {
 
 #' @export
 #' @method predict blblm
+#' @param object an object of class blblm
+#' @param new_data the new data to apply the blblm model to
+#' @param confidence a boolean indicating whether the confidence interval is desired
+#' @param level a double between 0 and 1 indicating alpha level
+#' @return fitted values for the new_data according to the blblm estimates
 predict.blblm <- function(object, new_data, confidence = FALSE, level = 0.95, ...) {
   est <- object$estimates
   X <- model.matrix(reformulate(attr(terms(object$formula), "term.labels")), new_data)
--- tests/testthat/test-fit.R
+++ tests/testthat/test-fit.R
@@ -1,3 +1,43 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("blblm works with dataset", {
+  a = blblm(speed ~ dist, cars, B = 10)
+  expect_lte(a$estimates$`3`[[3]]$sigma, 10)
+  expect_gt(a$estimates$`3`[[3]]$sigma, 0)
+  #learned the trick where you use multiple $ in 141B
+  expect_length(a$estimates, 10)
+  expect_named(a, c("estimates", "formula"), ignore.case = TRUE)
+})
+
+
+test_that("blblm works with files", {
+  car1 <- write.csv(cars, "car1.csv", row.names = FALSE)
+  car2 <- write.csv(cars, "car2.csv", row.names = FALSE)
+  b = blblm(speed ~ dist, c("car1.csv", "car2.csv"), B = 10)
+  expect_lte(b$estimates[[2]][[8]]$sigma, 8)
+  expect_gt(b$estimates[[1]][[1]]$coef[1], 0)
+  expect_length(b$estimates[[2]], 10)
+  expect_named(b, c("estimates", "formula"), ignore.case = TRUE)
+  file.remove(c("car1.csv", "car2.csv"))
+})
+
+test_that("blblm works with log reg", {
+  iris1 = iris[1:100,]
+  iris1$Species <- as.numeric(iris1$Species)-1 #the -1 takes all the numbers from 1 or 2 to 0 or 1
+  cc = blblm(Species ~ Sepal.Length, iris1, B = 50, m = 2, model_choice = "glm")
+  expect_lte(cc$estimates[[2]][[8]]$sigma, 1)
+  #expect_gt(blbsigma(cc, 0)) NOT THE SAME CLASS
+  expect_length(cc$estimates[[2]], 50)
+  expect_named(cc, c("estimates", "formula"), ignore.case = TRUE)
+  expect_equal(cc$formula, Species ~ Sepal.Length)
+})
+
+
+test_that("blblm works with log reg on 2 clusters", {
+  iris1 = iris[1:100,]
+  iris1$Species <- as.numeric(iris1$Species)-1 #the -1 takes all the numbers from 1 or 2 to 0 or 1
+  d = blblm(Species ~ Sepal.Length, iris1, B = 50, m = 2, model_choice = "glm", parallelization = TRUE, spec = 2)
+  expect_lte(d$estimates[[2]][[8]]$sigma, 1)
+  expect_lt(d$estimates[[1]][[1]]$coef[1], 0)
+  expect_length(d$estimates[[2]], 50)
+  expect_named(d, c("estimates", "formula"), ignore.case = TRUE)
+  expect_equal(d$formula, Species ~ Sepal.Length)
 })
